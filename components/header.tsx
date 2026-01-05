0bdd6ad973355b0059cd1a1a4d93bb892355ad67
Andrew Ball <andy@silverbackmarketing.com>
2026-01-04 19:08:45 -0700

    Remove desktop utility bar
    - Remove DesktopUtilityBar from Header component
    - Delete components/desktop-utility-bar.tsx
    - Header now contains only Navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,10 @@
 "use client"
 
-import { DesktopUtilityBar } from "@/components/desktop-utility-bar"
 import { Navigation } from "@/components/navigation"
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 bg-white shadow-sm">
-      <DesktopUtilityBar />
       <Navigation />
     </header>
   )
